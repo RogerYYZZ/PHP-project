750b5cb045dbb8ed9f71845a170c215ede8985fb
sam Ruby <zhemingyang2010@gmail.com>
2016-06-19 00:02:51 -0400

    Html for single post

---
+++ application/views/single_post.php
@@ -0,0 +1,23 @@
+
+
+<div class="container">
+
+	<div class="row">
+		<div  class="col-md-8">
+			<div class="panel panel-default" style = "margin-top:30px">
+				<div class="panel-heading" >
+					<h2><?php echo $single_post->title; ?></h2>
+				</div>
+				<div class="panel-body">
+					<?php echo $single_post->content; ?>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+
+
+
+
+</div>
