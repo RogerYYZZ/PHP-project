06fac8dab0ddb63ae658b6d7bb22df1ce782bf7d
sam Ruby <zhemingyang2010@gmail.com>
2016-06-21 00:37:40 -0400

    change config

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://localhost/personal/';
-$config['base_url'] = 'https://dry-chamber-28908.herokuapp.com/';
+$config['base_url'] = 'http://localhost/personal/';
+//$config['base_url'] = 'https://dry-chamber-28908.herokuapp.com/';
 /*
 |--------------------------------------------------------------------------
 | Index File
@@ -370,7 +370,8 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+//$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
