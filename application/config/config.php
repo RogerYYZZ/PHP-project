2e55816f780982b33fecd709eb3912b69222bc1a
sam Ruby <zhemingyang2010@gmail.com>
2016-06-21 00:28:28 -0400

    change base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/personal/';
-//$config['base_url'] = '';
+//$config['base_url'] = 'http://localhost/personal/';
+$config['base_url'] = 'https://dry-chamber-28908.herokuapp.com/';
 /*
 |--------------------------------------------------------------------------
 | Index File
