a3421b80a7adf7a50c7e1a9d757145bdefc2016d
sam Ruby <zhemingyang2010@gmail.com>
2016-06-24 16:34:30 -0400

    Js for adding new comment

---
+++ public/js/comment.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+		
+		$("button").click(function(){
+            // if(<?php !isset($user)?>)
+            //         window.location.href = "<?php base_url();?>";
+            // else{
+			  var content = $(this).parent().parent().find(".form-group").find("input").val();
+              var form = $(this).parent().parent();
+			  var com_id = $(this).parent().parent().find(".form-group").find("input").attr('id');
+			  var id = com_id.split("_")[1];
+              var currentdate = new Date();
+              var datetime;
+              var month = currentdate.getMonth()+1;
+            //  form.reset();
+            
+                datetime = ((month >= 1 && month <= 9)? ("0"+month):(month))+"-"+((currentdate.getDate()>=0 && currentdate.getDate() <= 9)?("0"+currentdate.getDate()):(currentdate.getDate()))+" "+((currentdate.getHours() >= 0 && currentdate.getHours() <= 9)? ("0"+currentdate.getHours()):(currentdate.getHours()))+":"+((currentdate.getMinutes() >= 0 && currentdate.getMinutes() <= 9)? ("0"+currentdate.getMinutes()):(currentdate.getMinutes()));
+              
+
+
+			if(content != ''){
+					$.ajax({
+					type: "POST",
+					url: BASE_URL+"content/comment",
+					data: {'id':id,'comment':content},
+					cache: false,
+                    dataType : 'JSON',
+					success: function(data){
+					   if(data != "false"){
+                            $('#commentList_'+id).append("<li><div class='commenterImage'><p>"+data["username"]+":"+"</p></div><div class='commentText'><p>"+content+"</p><span class='date sub-text'>"+datetime+"</span></div></li>");
+                            form[0].reset();
+                       }
+                        
+                 //    alert(form.attr('role'));
+					}
+					//return false;
+					});
+			}
+        
+		
+
+		});
+            
+        
+
+	});
