15e083dbc293d1810c7cf3085a9874b0f1fcb5a0
sam Ruby <zhemingyang2010@gmail.com>
2016-06-24 16:16:10 -0400

    Style the specific post page

--- application/views/single_post.php
+++ application/views/single_post.php
@@ -1,4 +1,12 @@
 
+<head>
+<link href="<?php echo base_url();?>public/css/comment.css" rel="stylesheet"></link>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script type="text/javascript">
+    var BASE_URL = "<?php echo base_url();?>";
+</script>
+<script src="<?php echo base_url();?>public/js/comment.js"></script>
+</head>
 
 <div class="container">
 
@@ -11,13 +19,46 @@
 				<div class="panel-body">
 					<?php echo $single_post->content; ?>
 				</div>
+
+			 <hr>
+
+        	<div class="detailBox" >
+    			<div class="titleBox">
+      				<label>Comment</label>
+        <!-- <button type="button" class="close" aria-hidden="true">&times;</button> -->
+    			</div>
+   
+    			<div class="actionBox">
+        			<ul class="commentList" id = "commentList_<?php echo  $single_post->post_id; ?>">
+        				<?php foreach($single_comment as $row): ?>
+        				   <li>
+                				<div class="commenterImage">
+                  <!-- <img src="http://lorempixel.com/50/50/people/6" /> -->
+                  					<p> <?php echo $row->username;?>: </p>
+                				</div>
+                				<div class="commentText">
+                    				<p class=""><?php echo $row->comment_content;?></p> <span class="date sub-text"><?php echo $row->minutes;?></span>
+
+                				</div>
+            				</li>
+            			<?php  endforeach;?>
+        			</ul>
+        			<form class="form-inline" role="form" >
+           				 <div class="form-group">
+                			<input class="form-control" id = "comment_<?php echo $single_post->post_id ?>" type="text" placeholder="Your comments" required >
+            			</div>
+            			<div class="form-group">
+                			<button class="btn btn-default" type = "button">Add</button>
+            			</div>
+        			</form>
+    			</div>
+
 			</div>
-		</div>
+
+			</div>
+
+
+	 	</div>
 
 	</div>
-
-
-
-
-
 </div>
