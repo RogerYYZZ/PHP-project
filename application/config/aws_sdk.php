13da920854d21fecbb1519108ba9fda597070072
sam Ruby <zhemingyang2010@gmail.com>
2016-07-01 18:44:25 -0400

    Aws s3 config

---
+++ application/config/aws_sdk.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['aws_access_key']="AKIAJL7IRKQETTRJ7OJQ";
+$config['aws_secret_key']="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";
