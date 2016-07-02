8c3a6cb9d0940ed278d13ca73df5629bf06cea06
sam Ruby <zhemingyang2010@gmail.com>
2016-07-01 20:10:46 -0400

    Test image upload function

--- application/views/head.php
+++ application/views/head.php
@@ -7,7 +7,7 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"></script> 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="https://malsup.github.com/jquery.form.js"></script> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.js"></script> 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
 
