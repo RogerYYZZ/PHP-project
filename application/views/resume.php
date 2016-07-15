23af777793f7b3bf7128cded1e98fce43daa0350
sam Ruby <zhemingyang2010@gmail.com>
2016-07-14 23:10:24 -0400

    My resume

---
+++ application/views/resume.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Document</title>
+  <style>
+
+  p{
+    font-size: 16px;
+  }
+
+  .degree{
+    font-size: 14px;
+  }
+
+  h3{
+    color: rgba(46, 127, 205, 1);
+    margin-top:50px;
+  }
+  span{
+    font-size: 14px;
+  }
+  .list_date .list{
+      display:inline-block;
+  }
+  .list_date .date{
+    display:inline-block;
+    position:absolute; 
+    right:0;
+   
+    font-family:verdana;
+    font-size:14px;
+  }
+</style>
+  <script>
+</script>
+</head>
+	
+<div class="container" style="margin-bottom: 60px">
+    <div class="col-md-10 col-md-offset-1">
+        <h3>Zheming Yang</h3>
+        <hr>
+        <p>14 Berkshire St Apt #1, Worcester MA 01609</p>
+        <p>508-797-7300</p>
+        <p>zhemingyang2010@gmail.com</p>
+        <p>Github: <a href="https://github.com/RogerYYZZ/">https://github.com/RogerYYZZ/</a></p>
+        <p>Linkedin: <a href="https://www.linkedin.com/in/zhemingyang">https://www.linkedin.com/in/zhemingyang</a></p>
+        <h3>Education</h3>
+        <hr>
+       
+         <div class="list_date"><p class="list"><strong>Worcester Polytechnic Institute (WPI),</strong> Worcester, MA</p><p class="date">May 2016</p></div>
+         <p class="degree"><strong>Master of Science, Electrical and Computer Engineering, </strong>GPA: 3.4</p>
+         <p class="degree"><strong>Relevant coursework: </strong>Database Management Systems; Introduction to Algorithms; Design of Software System; Introduction to Local and Wide Area Networks; Analysis of probabilistic and Systems;</p>
+         <br>
+           <div class="list_date"><p class="list"><strong>Beijing Institute of Technology,</strong> Beijing, China</p><p class="date">June 2014</p></div>
+         <p class="degree"><strong>Bachelor of Science, Communication Engineering, </strong>GPA: 3.3</p>
+         <p class="degree"><strong>Relevant coursework: </strong>Data Structure and Algorithm Design; Digital Circuit; C Programming;</p>
+
+         <h3>Skills</h3>
+         <hr>
+         <p><strong>Programming language: </strong>Java(proficient), PHP(proficient), JavaScript(proficient), JQuery, HTML/CSS, C, C++, SQL, Python</p>
+         <p><strong>Concept: </strong>Ajax, Apache Tomcat, Codeigniter, AWS, Heroku, XML, JSON, Rails, Spring MVC, bootstrap, git, TCP/IP</p>
+         <p><strong>Software: </strong>Eclipse, Visual Studio, Xcode, Office, Modelsim, Labview</p>
+         <p><strong>Platform: </strong>Windows, OS X, Oracle, MySQL, Postgres, Linux</p>
+
+         <h3>Projects</h3>
+         <hr>
+         <div class="list_date"><p class="list"><strong>Web application for flight reservation system, WPI</strong></p><p class="date">Feb 2015-May 2015</p></div>
+         <ul>
+            <li><p>Designed and implemented online flight tickets system using Java</p></li>
+            <li><p>Achieved the front-end using HTML/CSS, JavaScript which can let users to query flights</p></li>
+            <li><p>Utilized Java Servlet to receive the HTTP request and interact with server</p></li>
+            <li><p>Performed JUnit and integration test in Mac OS environment</p></li>
+            <li><p>Github: <a href="https://github.com/RogerYYZZ/cs509/tree/master/cs509.hobbits">https://github.com/RogerYYZZ/cs509/tree/master/cs509.hobbits</a></p></li>
+          </ul>
+          <iframe width="500" height="345" src="http://www.youtube.com/embed/muTFqaLaqWI/" style="display: block; margin: auto; margin-top:30px"></iframe>
+          <hr>
+           <div class="list_date"><p class="list"><strong>‘ASSISTments’ APP for Edmodo classroom, WPI</strong></p><p class="date">Sep 2015-Dec 2015</p></div>
+         <ul>
+            <li><p>Added new features for ‘ASSISTments’ APP to create assignments to google and Edmodo</p></li>
+            <li><p>Implemented new Java classes using Edmodo API and ‘ASSISTments’ API to set new class and several properties of this class</p></li>
+            <li><p>Completed new features to set grades for specific assignment and create teacher report</p></li>
+            <li><p>Built front-end frame by bootstrap and JQuery</p></li>
+          </ul>
+          <hr>
+            <div class="list_date"><p class="list"><strong>PHP blog web app, WPI </strong></p><p class="date">Apr 2016-June 2016</p></div>
+         <ul>
+            <li><p>Utilized Codeigniter web framework to develop whole blog project and the architectural pattern is MVC</p></li>
+            <li><p>Implemented the function that everyone can post stories and articles and share them to their friends</p></li>
+            <li><p>Added comments feature for every posts to make users share their thoughts</p></li>
+            <li><p>Github: <a href="https://github.com/RogerYYZZ/PHP-project">https://github.com/RogerYYZZ/PHP-project</a></p></li>
+          </ul>
+            <hr>
+            <div class="list_date"><p class="list"><strong>Arduino project to control the move of car by voice, BIT </strong></p><p class="date">Nov 2013-Dec 2013 </p></div>
+         <ul>
+            <li><p>Developed the algorithms using C programming to achieve the car moved</p></li>
+            <li><p>Established control system in Arduino board</p></li>
+            <li><p>Executed integrate test to guarantee the car can be operated accurately</p></li>
+          </ul>
+          <h3>Experience</h3>
+         <hr>
+          <div class="list_date"><p class="list"><strong>Software Engineer Intern, China Unicom, Beijing, China</strong></p><p class="date">June 2013-Aug 2013</p></div>
+            <ul>
+            <li><p>Managed customized data of communication control by MySQL</p></li>
+            <li><p>Tested monitoring software and documented software specifications</p></li>
+          </ul>
+
+       <!--  <button style="display: block; margin: auto; margin-top:50px" class="btn btn-primary">Dowmload my resume</button> -->
+
+    </div>
+</div>
+<footer class="footer" >
+      <div class="container">
+        <p class="text-muted">Copyright © 2016 Zheming</p>
+      </div>
+  </footer>
+
+</body>
+</html>
