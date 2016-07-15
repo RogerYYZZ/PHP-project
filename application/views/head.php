5a4fa4cfd0f8cf3b5271006059d4c417eb10273c
sam Ruby <zhemingyang2010@gmail.com>
2016-07-14 23:11:41 -0400

    Style change

--- application/views/head.php
+++ application/views/head.php
@@ -26,6 +26,7 @@ max-height: 20%; /* or to whatever you want here */
 </head> 
 
 <body>
+ 
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -43,27 +44,32 @@ max-height: 20%; /* or to whatever you want here */
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="<?php echo base_url();  ?>user/content">Read</a></li>
+        <li class = "dropdown"><a  href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Read<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+           <li><a href="<?php echo base_url();  ?>user/content">Friend posts</a></li>
+            <li><a  href="<?php echo base_url();?>zhihu">Zhihu daily</a></li>
+            <li><a href="#">Something else here</a></li>
+          </ul>
+        </li>
         <li><a href="<?php echo base_url();  ?>user/post">Post</a></li>
-    <!--     <li class="dropdown">
+        <li><a href="<?php echo base_url();  ?>resume">about</a></li>
+     <!--    <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
+           >
             <li role="separator" class="divider"></li>
             <li><a href="#">Separated link</a></li>
             <li role="separator" class="divider"></li>
             <li><a href="#">One more separated link</a></li>
           </ul>
-        </li> -->
-      </ul>
-      <form class="navbar-form navbar-left" id="search1" role="search"  style="margin-left: 10%">
+        </li>
+ -->      </ul>
+   <!--    <form class="navbar-form navbar-left" id="search1" role="search"  style="margin-left: 10%">
         <div class="form-group">
           <input type="text" class="form-control" id=" form-control1" placeholder="Search" style="width: 300px;">
         </div>
         <button type="submit" class="btn btn-default">Submit</button>
-      </form>
+      </form> -->
       <ul class="nav navbar-nav navbar-right">
         
         <li class="dropdown">
@@ -88,4 +94,5 @@ max-height: 20%; /* or to whatever you want here */
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
+
 
