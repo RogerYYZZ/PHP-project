3906001eca676adb9d32c4ee2239268e2b703d97
sam Ruby <zhemingyang2010@gmail.com>
2016-07-14 23:09:03 -0400

    Class to process call from 'zhihu daily'

---
+++ application/controllers/News.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+	
+	class News extends CI_Controller {
+
+		public function __construct()
+		{	
+			parent::__construct();
+			$this->load->library('session');
+			$this->load->helper('url');
+
+		}
+
+		public function get_zhihu(){
+			
+			$this->load->library('curl');
+			$result = json_decode($this->curl->simple_get('http://news-at.zhihu.com/api/4/news/latest'));
+			foreach($result->stories as $key => $row){
+				$full_content = json_decode($this->curl->simple_get('http://news-at.zhihu.com/api/4/news/'.$row->id));
+				$result->stories[$key]->full_content = $full_content;
+			}
+
+			$data["user"] = $this->session->userdata("username");
+			$data['article'] =  $result;
+			// $this->output->cache(300);
+			$this->load->view('head',$data);
+			$this->load->view("zhihu",$data);
+			
+
+		}
+
+
+
+
+
+
+
+
+	}
